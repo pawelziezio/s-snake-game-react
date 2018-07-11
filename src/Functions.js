ad38ebdec0dee56f6ad868d207c917d0d9b5b7d2
pawelziezio <pawel.ziezio@yahoo.com>
2018-07-11 14:14:48 +0200

    mines(skulls)- every 30 seconds ...

--- src/Functions.js
+++ src/Functions.js
@@ -6,3 +6,10 @@ export function collisionTest(x,y,array){
         }
         return false;
     };
+
+export function randomPositionXY(maxX, maxY ){
+        return [
+            Math.floor(Math.random()*maxX),
+            Math.floor(Math.random()*maxY)
+        ]
+    };
